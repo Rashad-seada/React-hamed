fd09149971b1d1a583e437cb0a862f9d8d318d3a
Rashad <rashadatef2@gmail.com>
2025-12-23 08:12:55 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // Restart
-
+  plugins: [react()],
+  preview: {
+    allowedHosts: ["hamed.low-high.org"]
+  }
 })
